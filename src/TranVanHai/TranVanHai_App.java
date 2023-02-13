81b86d9c3572b32b84c9f52dcb31e1e87b4a0c4d
TranHai2003 <hai291203@gmail.com>
2023-02-13 22:11:31 +0700

    first commit

---
+++ src/TranVanHai/TranVanHai_App.java
@@ -0,0 +1,8 @@
+package TranVanHai;
+
+public class TranVanHai_App {
+    public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Tran Van Hai hoc Java");
+	}
+}
